48637f2dd917898b74ed8aa02a1f14bac1eab0c0
junseokhub <github.yam117@passinbox.com>
2026-08-03 12:49:42 +0900

    feat: [common-web] add micrometer-registry-prometheus

--- libs/common-web/build.gradle.kts
+++ libs/common-web/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
 
 dependencies {
     api("org.springframework.boot:spring-boot-starter-web")
+    api("io.micrometer:micrometer-registry-prometheus")
     compileOnly("org.springframework.boot:spring-boot-autoconfigure")
     implementation("org.springframework.boot:spring-boot-starter-security")
 }
